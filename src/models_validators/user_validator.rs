db8bed3d0ff48bc8e8e214892e41b590e9344a8b
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-07 23:28:10 +0300

    Made a DTO for UpdateUserData and created a TryFrom<HashMap<String, serde_json::Value>> function for it with detailed validation

--- src/models_validators/user_validator.rs
+++ src/models_validators/user_validator.rs
@@ -119,3 +119,106 @@ impl TryFrom<HashMap<String, serde_json::Value>> for InsertUserData {
         }
     }
 }
+
+#[derive(Debug, Serialize)]
+pub struct UpdateUserData {
+    pub name: Option<String>,
+    pub gender: Option<Gender>,
+    pub role: Option<UserRole>,
+    pub bio: Option<String>,
+    pub email: Option<String>,
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub activated: Option<bool>,
+    pub verified: Option<bool>,
+}
+
+impl TryFrom<HashMap<String, serde_json::Value>> for UpdateUserData {
+    type Error = ApiErrorType;
+
+    fn try_from(value: HashMap<String, serde_json::Value>) -> Result<Self, Self::Error> {
+        let mut errors = Vec::<ValidationError>::new();
+
+        let name = validate_string_field(
+            &value.get("name"),
+            "name",
+            FieldLength {
+                min: None,
+                max: Some(100),
+            },
+            &mut errors,
+            true,
+        );
+
+        let email = validate_email_field(
+            &value.get("email"),
+            "email",
+            FieldLength {
+                min: None,
+                max: Some(100),
+            },
+            &mut errors,
+            true,
+        );
+
+        let bio = validate_string_field(
+            &value.get("bio"),
+            "bio",
+            FieldLength {
+                min: None,
+                max: Some(255),
+            },
+            &mut errors,
+            true,
+        );
+
+        let username = validate_string_field(
+            &value.get("username"),
+            "username",
+            FieldLength {
+                min: None,
+                max: Some(100),
+            },
+            &mut errors,
+            true,
+        );
+
+        let password = validate_string_field(
+            &value.get("password"),
+            "password",
+            FieldLength {
+                min: None,
+                max: Some(100),
+            },
+            &mut errors,
+            true,
+        );
+
+        let verified =
+            validate_boolean_field(&value.get("verified"), "verified", &mut errors, true);
+
+        let activated =
+            validate_boolean_field(&value.get("activated"), "activated", &mut errors, true);
+
+        let gender = validate_gender_enum_field(&value.get("gender"), "gender", &mut errors, true);
+
+        let user_role =
+            validate_user_role_enum_field(&value.get("userRole"), "userRole", &mut errors, true);
+
+        if errors.is_empty() {
+            Ok(UpdateUserData {
+                name,
+                email,
+                username,
+                password,
+                gender,
+                bio,
+                role: user_role,
+                verified,
+                activated,
+            })
+        } else {
+            Err(ApiErrorType::BodyValidationErrors(errors))
+        }
+    }
+}

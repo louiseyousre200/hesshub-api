3ef8f9c0973ce650d2608b6c1896e298f0f3c705
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-07 21:13:05 +0300

    Added utility structs, enums andfunctions related to validation and responding.

---
+++ src/utils/response.rs
@@ -0,0 +1,182 @@
+use std::num::IntErrorKind;
+
+use serde_derive::{Deserialize, Serialize};
+use serde_json::json;
+use warp::hyper::StatusCode;
+
+use super::validator::{FieldLength, FieldType};
+
+#[derive(Debug, Serialize, Clone, Copy, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ApiResource {
+    Users,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ValidationError {
+    InvalidJsonBody,
+    RequiredFieldMissing {
+        field_name: String,
+    },
+    InvalidFieldDataType {
+        field_name: String,
+        expected_type: FieldType,
+    },
+    InvalidFieldContentLength {
+        field_name: String,
+        passed_length: usize,
+        expected_length: FieldLength,
+    },
+    IncorrectEnumValue {
+        field_name: String,
+        passed_value: String,
+        expected_values: Vec<String>,
+    },
+    InvalidEmailFormat {
+        passed_value: String,
+    },
+    InvalidTelephoneFormat {
+        passed_value: String,
+    },
+}
+
+#[derive(Debug)]
+pub enum ApiErrorType {
+    // Not Found
+    ResourceNotFound(ApiResource),
+    RouteNotFound,
+
+    // Authentication & Authorization
+    Unauthorized,
+    NotLoggedIn,
+    InvalidJwtToken,
+    InvalidCredentials,
+
+    // Unidentifiable
+    InternalServerError,
+
+    // Generic Validation
+    BodyValidationErrors(Vec<ValidationError>),
+
+    // Query Validation
+    InvalidSortingQuerySyntax,
+    NonExistantSortingQueryField(String),
+    InvalidPaginationPageQueryField(IntErrorKind),
+    InvalidPaginationSizeQueryField(IntErrorKind),
+
+    // Parameters Validation
+    InvalidIdParam(ApiResource),
+
+    // Already existing
+    AlreadyExists(ApiResource),
+
+    // File Uploading
+    NotAnImage,
+    UnnamedMultipartFile,
+    EmptyFile,
+    NoImage,
+}
+
+fn int_error_kind_to_str(kind: &IntErrorKind) -> Option<String> {
+    match kind {
+        IntErrorKind::Empty => Some("EMPTY".to_string()),
+        IntErrorKind::InvalidDigit => Some("INVALID_DIGIT".to_string()),
+        IntErrorKind::PosOverflow => Some("POS_OVERFLOW".to_string()),
+        IntErrorKind::NegOverflow => Some("NEG_OVERFLOW".to_string()),
+        IntErrorKind::Zero => Some("ZERO".to_string()),
+        _ => None,
+    }
+}
+
+impl ApiErrorType {
+    pub fn status_code(&self) -> StatusCode {
+        match self {
+            ApiErrorType::Unauthorized => StatusCode::FORBIDDEN,
+            ApiErrorType::InvalidCredentials
+            | ApiErrorType::NotLoggedIn
+            | ApiErrorType::InvalidJwtToken => StatusCode::UNAUTHORIZED,
+            ApiErrorType::RouteNotFound | ApiErrorType::ResourceNotFound(_) => {
+                StatusCode::NOT_FOUND
+            }
+            ApiErrorType::NoImage
+            | ApiErrorType::EmptyFile
+            | ApiErrorType::NotAnImage
+            | ApiErrorType::UnnamedMultipartFile
+            | ApiErrorType::InvalidIdParam(_)
+            | ApiErrorType::BodyValidationErrors(_)
+            | ApiErrorType::InvalidPaginationPageQueryField(_)
+            | ApiErrorType::InvalidPaginationSizeQueryField(_)
+            | ApiErrorType::InvalidSortingQuerySyntax
+            | ApiErrorType::NonExistantSortingQueryField(_) => StatusCode::BAD_REQUEST,
+            ApiErrorType::AlreadyExists(_) => StatusCode::CONFLICT,
+            ApiErrorType::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    pub fn details(&self) -> Option<serde_json::Value> {
+        match self {
+            ApiErrorType::NoImage
+            | ApiErrorType::UnnamedMultipartFile
+            | ApiErrorType::EmptyFile
+            | ApiErrorType::NotAnImage
+            | ApiErrorType::RouteNotFound
+            | ApiErrorType::Unauthorized
+            | ApiErrorType::NotLoggedIn
+            | ApiErrorType::InvalidJwtToken
+            | ApiErrorType::InvalidCredentials
+            | ApiErrorType::InternalServerError
+            | ApiErrorType::InvalidSortingQuerySyntax => None,
+            ApiErrorType::InvalidIdParam(details) => Some(json!(details)),
+            ApiErrorType::ResourceNotFound(details) => Some(json!(details)),
+            ApiErrorType::BodyValidationErrors(details) => Some(json!(details)),
+            ApiErrorType::NonExistantSortingQueryField(details) => Some(json!(details)),
+            ApiErrorType::InvalidPaginationPageQueryField(details) => {
+                Some(json!(int_error_kind_to_str(details)))
+            }
+            ApiErrorType::InvalidPaginationSizeQueryField(details) => {
+                Some(json!(int_error_kind_to_str(details)))
+            }
+            ApiErrorType::AlreadyExists(details) => Some(json!(details)),
+        }
+    }
+
+    pub fn code<'a>(&self) -> &'a str {
+        match self {
+            ApiErrorType::ResourceNotFound(_) => "RESOURCE_NOT_FOUND",
+            ApiErrorType::RouteNotFound => "ROUTE_NOT_FOUND",
+            ApiErrorType::Unauthorized => "UNAUTHORIZED",
+
+            ApiErrorType::BodyValidationErrors(_) => "BODY_VALIDATION_ERRORS",
+            ApiErrorType::InvalidSortingQuerySyntax => "INVALID_SORTING_QUERY_SYNTAX",
+            ApiErrorType::NonExistantSortingQueryField(_) => "INVALID_SORTING_QUERY_FIELD",
+            ApiErrorType::InvalidPaginationPageQueryField(_) => {
+                "INVALID_PAGINATION_PAGE_QUERY_FIELD"
+            }
+            ApiErrorType::InvalidPaginationSizeQueryField(_) => {
+                "INVALID_PAGINATION_SIZE_QUERY_FIELD"
+            }
+
+            ApiErrorType::NotLoggedIn => "NOT_LOGGED_IN",
+            ApiErrorType::InvalidJwtToken => "INVALID_JWT_TOKEN",
+            ApiErrorType::InvalidCredentials => "INVALID_CREDENTIALS",
+
+            ApiErrorType::AlreadyExists(_) => "ALREADY_EXISTS",
+
+            ApiErrorType::InternalServerError => "INTERNAL_SERVER_ERROR",
+            ApiErrorType::InvalidIdParam(_) => "INVALID_ID_PARAM",
+            ApiErrorType::UnnamedMultipartFile => "UNNAMED_MULTIPART_FILE",
+            ApiErrorType::NotAnImage => "NOT_AN_IMAGE",
+            ApiErrorType::EmptyFile => "EMPTY_FILE",
+            ApiErrorType::NoImage => "NO_IMAGE",
+        }
+    }
+}
+
+impl warp::reject::Reject for ApiErrorType {}
+
+#[derive(Debug, Serialize)]
+pub struct ApiErrorResponse<T: serde::Serialize> {
+    pub code: String,
+    pub details: T,
+}
---
+++ src/utils/validator.rs
@@ -0,0 +1,209 @@
+use regex::Regex;
+use serde_derive::Serialize;
+use std::collections::HashMap;
+
+use super::response::ValidationError;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum FieldType {
+    Array,
+    Bool,
+    Null,
+    Number,
+    Object,
+    String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct FieldLength {
+    pub min: Option<usize>,
+    pub max: Option<usize>,
+}
+
+pub fn validate_email_field(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    field_length: FieldLength,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+) -> Option<String> {
+    let email_regex = Regex::new(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$").unwrap();
+
+    let email_value = validate_string_field(value, name, field_length, errors, optional);
+
+    if let Some(email) = &email_value {
+        if !email_regex.is_match(email.as_str()) {
+            errors.push(ValidationError::InvalidEmailFormat {
+                passed_value: email.to_string(),
+            });
+        }
+    }
+
+    email_value
+}
+
+pub fn validate_telephone_number_field(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    field_length: FieldLength,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+) -> Option<String> {
+    let phone_number_regex =
+        Regex::new(r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$").unwrap();
+
+    let telephone_number = validate_string_field(value, name, field_length, errors, optional);
+
+    if let Some(telephone_number) = &telephone_number {
+        if !phone_number_regex.is_match(telephone_number.as_str()) {
+            errors.push(ValidationError::InvalidTelephoneFormat {
+                passed_value: telephone_number.to_string(),
+            });
+        }
+    }
+
+    telephone_number
+}
+
+pub fn validate_enum_field<E>(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    expected_values: &HashMap<E, String>,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+) -> Option<E>
+where
+    E: PartialEq + Clone,
+{
+    match value {
+        Some(serde_json::Value::String(value)) => {
+            for (enum_value, str_value) in expected_values {
+                if str_value == value {
+                    return Some(enum_value.clone());
+                }
+            }
+            errors.push(ValidationError::IncorrectEnumValue {
+                field_name: name.to_string(),
+                passed_value: value.to_string(),
+                expected_values: expected_values.values().cloned().collect(),
+            });
+            None
+        }
+        None => {
+            if !optional {
+                errors.push(ValidationError::RequiredFieldMissing {
+                    field_name: name.to_string(),
+                });
+            }
+            None
+        }
+        _ => {
+            errors.push(ValidationError::InvalidFieldDataType {
+                field_name: name.to_string(),
+                expected_type: FieldType::String,
+            });
+            None
+        }
+    }
+}
+
+pub fn validate_boolean_field(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+) -> Option<bool> {
+    match value {
+        Some(serde_json::Value::Bool(value)) => Some(value.to_owned()),
+        None => {
+            if !optional {
+                errors.push(ValidationError::RequiredFieldMissing {
+                    field_name: name.to_string(),
+                });
+            }
+            None
+        }
+        _ => {
+            errors.push(ValidationError::InvalidFieldDataType {
+                field_name: name.to_string(),
+                expected_type: FieldType::Bool,
+            });
+            None
+        }
+    }
+}
+
+pub fn validate_string_field(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    field_length: FieldLength,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+) -> Option<String> {
+    match value {
+        Some(serde_json::Value::String(value)) => {
+            let len = value.len();
+            if let Some(min) = field_length.min {
+                if len < min {
+                    errors.push(ValidationError::InvalidFieldContentLength {
+                        field_name: name.to_string(),
+                        passed_length: len,
+                        expected_length: FieldLength {
+                            min: Some(min),
+                            max: field_length.max,
+                        },
+                    });
+                    None
+                } else if let Some(max) = field_length.max {
+                    if len > max {
+                        errors.push(ValidationError::InvalidFieldContentLength {
+                            field_name: name.to_string(),
+                            passed_length: len,
+                            expected_length: FieldLength {
+                                min: field_length.min,
+                                max: Some(max),
+                            },
+                        });
+                        None
+                    } else {
+                        Some(value.to_string())
+                    }
+                } else {
+                    Some(value.to_string())
+                }
+            } else if let Some(max) = field_length.max {
+                if len > max {
+                    errors.push(ValidationError::InvalidFieldContentLength {
+                        field_name: name.to_string(),
+                        passed_length: len,
+                        expected_length: FieldLength {
+                            min: field_length.min,
+                            max: Some(max),
+                        },
+                    });
+                    None
+                } else {
+                    Some(value.to_string())
+                }
+            } else {
+                Some(value.to_string())
+            }
+        }
+        None => {
+            if !optional {
+                errors.push(ValidationError::RequiredFieldMissing {
+                    field_name: name.to_string(),
+                });
+            }
+            None
+        }
+        _ => {
+            errors.push(ValidationError::InvalidFieldDataType {
+                field_name: name.to_string(),
+                expected_type: FieldType::String,
+            });
+            None
+        }
+    }
+}

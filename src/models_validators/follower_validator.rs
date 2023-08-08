13aa43eb1979460131c753530f7c58d906bce31d
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-08 20:14:35 +0300

    Made a DTO for InsertFollowerData and created a TryFrom<HashMap<String, serde_json::Value>> function for it with detailed validation

---
+++ src/models_validators/follower_validator.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+use serde_derive::Serialize;
+
+use crate::utils::{
+    response::{ApiErrorType, ValidationError},
+    validator::validate_boolean_field,
+};
+
+#[derive(Debug, Serialize)]
+pub struct InsertFollowerData {
+    pub watch_new_hesses: Option<bool>,
+    pub watch_replies: Option<bool>,
+    pub watch_follows: Option<bool>,
+    pub watch_likes: Option<bool>,
+}
+
+impl TryFrom<HashMap<String, serde_json::Value>> for InsertFollowerData {
+    type Error = ApiErrorType;
+
+    fn try_from(value: HashMap<String, serde_json::Value>) -> Result<Self, Self::Error> {
+        let mut errors = Vec::<ValidationError>::new();
+
+        let watch_follows = validate_boolean_field(
+            &value.get("watchFollows"),
+            "watchFollows",
+            &mut errors,
+            true,
+        );
+
+        let watch_likes =
+            validate_boolean_field(&value.get("watchLikes"), "watchLikes", &mut errors, true);
+
+        let watch_new_hesses = validate_boolean_field(
+            &value.get("watchNewHesses"),
+            "watchNewHesses",
+            &mut errors,
+            true,
+        );
+
+        let watch_replies = validate_boolean_field(
+            &value.get("watchReplies"),
+            "watchReplies",
+            &mut errors,
+            true,
+        );
+
+        if errors.is_empty() {
+            Ok(InsertFollowerData {
+                watch_follows,
+                watch_likes,
+                watch_new_hesses,
+                watch_replies,
+            })
+        } else {
+            Err(ApiErrorType::BodyValidationErrors(errors))
+        }
+    }
+}

afe9276abcc4ec3d338f9d87b5158344a88c1233
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-08 16:10:25 +0300

    Created a method to validate serde_json::Value as a correct vector (array) of WhoCan enum values with the option to accept null

--- src/models_validators/enums/who_can_validator.rs
+++ src/models_validators/enums/who_can_validator.rs
@@ -2,7 +2,10 @@ use std::collections::HashMap;
 
 use crate::{
     models::WhoCan,
-    utils::{response::ValidationError, validator::validate_enum_field},
+    utils::{
+        response::ValidationError,
+        validator::{validate_array_field, validate_enum_field},
+    },
 };
 
 pub fn validate_who_can_enum_field(
@@ -20,4 +23,30 @@ pub fn validate_who_can_enum_field(
     .collect();
 
     validate_enum_field(value, name, &who_can_values, errors, optional)
+}
+
+pub fn validate_who_can_enum_array_field(
+    value: &Option<&serde_json::Value>,
+    name: &str,
+    errors: &mut Vec<ValidationError>,
+    optional: bool,
+    nullable: bool,
+) -> Option<Option<Vec<WhoCan>>> {
+    let who_can = validate_array_field(value, name, errors, optional, nullable);
+
+    match who_can {
+        Some(Some(value)) => {
+            let mut res = value
+                .into_iter()
+                .map(|ref v| validate_who_can_enum_field(&Some(v), name, errors, false));
+
+            if res.any(|v| v.is_none()) {
+                None
+            } else {
+                Some(Some(res.map(Option::unwrap).collect()))
+            }
+        }
+        Some(None) => Some(None),
+        None => None,
+    }
 }

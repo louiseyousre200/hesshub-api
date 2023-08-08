d23d3d2365d681bd9c5d2e4a087393223d7a4be8
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-08 11:53:38 +0300

    Added documentation comments for user confirmation tokens services

--- src/services/user_confirmation_tokens_service.rs
+++ src/services/user_confirmation_tokens_service.rs
@@ -7,6 +7,20 @@ use crate::{
     utils::response::{ApiErrorType, ApiResource},
 };
 
+/// Retrieves a valid user confirmation token by its ID.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+/// * `id` - The UUID of the user confirmation token to retrieve.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the retrieved `UserConfirmationToken` if successful.
+/// If the token is expired, returns an `ApiErrorType::UserConfirmationTokenExpired` error.
+/// If no token is found, returns an `ApiErrorType::ResourceNotFound` error.
+/// If any other error occurs during the database query, returns an `ApiErrorType::InternalServerError` error.
+///
 pub async fn get_valid_user_confirmation_token_by_id(
     pool: Pool<Postgres>,
     id: Uuid,
@@ -31,6 +45,19 @@ pub async fn get_valid_user_confirmation_token_by_id(
     }
 }
 
+/// Inserts a new user confirmation token into the database.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+/// * `user_id` - The UUID of the user associated with the token.
+/// * `expire_at` - The expiration timestamp for the token.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the inserted `UserConfirmationToken` if successful.
+/// If any error occurs during database insertion, returns an `ApiErrorType::InternalServerError` error.
+///
 pub async fn insert_user_confirmation_token(
     pool: Pool<Postgres>,
     user_id: Uuid,
@@ -54,6 +81,17 @@ pub async fn insert_user_confirmation_token(
     }
 }
 
+/// Deletes expired user confirmation tokens from the database.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+///
+/// # Returns
+///
+/// Returns `Result<(), ApiErrorType>` indicating success or an `ApiErrorType::InternalServerError` error
+/// if any error occurs during database deletion.
+///
 pub async fn delete_expired_user_confirmation_tokens(
     pool: Pool<Postgres>,
 ) -> Result<(), ApiErrorType> {

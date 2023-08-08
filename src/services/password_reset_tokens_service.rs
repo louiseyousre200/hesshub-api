ce480a6a7a9b909e72d8709891d25a6988028cc3
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-08 11:53:15 +0300

    Added documentation comments to password reset tokens service

--- src/services/password_reset_tokens_service.rs
+++ src/services/password_reset_tokens_service.rs
@@ -7,6 +7,20 @@ use crate::{
     utils::response::{ApiErrorType, ApiResource},
 };
 
+/// Retrieves a valid password reset token by its ID.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+/// * `id` - The UUID of the password reset token to retrieve.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the retrieved `PasswordResetToken` if successful.
+/// If the token is expired, returns an `ApiErrorType::PasswordResetTokenExpired` error.
+/// If no token is found, returns an `ApiErrorType::ResourceNotFound` error.
+/// If any other error occurs during database query, returns an `ApiErrorType::InternalServerError` error.
+///
 pub async fn get_valid_password_reset_token_by_id(
     pool: Pool<Postgres>,
     id: Uuid,
@@ -31,6 +45,19 @@ pub async fn get_valid_password_reset_token_by_id(
     }
 }
 
+/// Inserts a new password reset token into the database.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+/// * `user_id` - The UUID of the user associated with the token.
+/// * `expire_at` - The expiration timestamp for the token.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the inserted `PasswordResetToken` if successful.
+/// If any error occurs during database insertion, returns an `ApiErrorType::InternalServerError` error.
+///
 pub async fn insert_password_reset_token(
     pool: Pool<Postgres>,
     user_id: Uuid,
@@ -54,6 +81,17 @@ pub async fn insert_password_reset_token(
     }
 }
 
+/// Deletes expired password reset tokens from the database.
+///
+/// # Arguments
+///
+/// * `pool` - A database connection pool.
+///
+/// # Returns
+///
+/// Returns `Result<(), ApiErrorType>` indicating success or an `ApiErrorType::InternalServerError` error
+/// if any error occurs during database deletion.
+///
 pub async fn delete_expired_password_reset_tokens(
     pool: Pool<Postgres>,
 ) -> Result<(), ApiErrorType> {

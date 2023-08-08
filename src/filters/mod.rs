e1d4a49a7036ad27da9bf99dac5cf807a35ecf40
louiseyousre200 <louiseyousre200@gmail.com>
2023-08-08 19:02:54 +0300

    Added required_uuid_param_filter and body_validation_filter to filters mod.rs

---
+++ src/filters/mod.rs
@@ -0,0 +1,5 @@
+mod body_validation_filter;
+mod required_uuid_param_filter;
+
+pub use body_validation_filter::body_validation_filter;
+pub use required_uuid_param_filter::required_uuid_param_filter;
